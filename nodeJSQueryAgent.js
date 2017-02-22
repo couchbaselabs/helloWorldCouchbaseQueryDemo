c5399a56782baa2f8b072920eed2a2fffb7db070
tonyjhillman <tonyjhillman@gmail.com>
2017-02-22 10:59:15 +0000

    Update nodeJSQueryAgent.js
    Modified to handle parameterised query.

--- nodeJSQueryAgent.js
+++ nodeJSQueryAgent.js
@@ -10,10 +10,12 @@ http.createServer(function (request, response)
 {
     console.log('New connection');
     
+    // Get the country-value, which has been passed by
+    // the client.
+    //
     var queryObject = url.parse(request.url, true).query
-	
-    var theQueryString = queryObject.myQuery;
-    console.log("Query string is " + theQueryString);
+    var countryValueString = queryObject.myCountryValue;
+    console.log("Country value is " + countryValueString);
     
     // Contact Couchbase and open the travel-sample bucket.
     //
@@ -21,24 +23,23 @@ http.createServer(function (request, response)
     var myCluster = new couchbase.Cluster('couchbase://localhost');
     var myBucket = myCluster.openBucket('travel-sample');
 
-    myBucket.get(theQueryString, function(err, res)  
-    {
-    	myBucket.manager().createPrimaryIndex(function()
-    	{
-    	    myBucket.query(
-                couchbase.N1qlQuery.fromString(theQueryString),
-    		function (err, rows) 
-    		{
-    		    console.log("Got rows: %j", rows);	
+    // Set up a parameterised N1QL query, whereby a placeholder is specified for the value
+    // of "country". This is the value that has been provided by the client.
+    //
+    var queryTemplate = couchbase.N1qlQuery.fromString('SELECT * FROM `travel-sample` WHERE country=$1');
+    
+    // Make the N1QL query, specifying the value provided by the client.
+    //
+    myBucket.query(queryTemplate, [countryValueString], 
+        function (err, rows) 
+        {
+            console.log("Got rows: %j", rows);	
     				
-    		    response.writeHead(200, {"Content-Type": "application/json", 
-    								"Access-Control-Allow-Origin": "*"});
-    		    console.log("Returning...");
-    		    response.end(JSON.stringify(rows));
-    	    });
-	});
-    });		
-									    
+            response.writeHead(200, {"Content-Type": "application/json", 
+                        "Access-Control-Allow-Origin": "*"});
+            console.log("Returning...");
+            response.end(JSON.stringify(rows));
+    });							    
 }).listen(8082);
 
 // Notify that the server is running and listening.
